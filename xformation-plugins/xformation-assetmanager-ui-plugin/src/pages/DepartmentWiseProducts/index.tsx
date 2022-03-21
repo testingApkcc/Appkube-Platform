fc69280bc1250e45977e369710dca23eff03225c
Satish Pal <satishweb93@gmail.com>
2022-03-21 12:57:41 +0530

    Department wise products page ui in assetmanager

--- xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/DepartmentWiseProducts/index.tsx
+++ xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/DepartmentWiseProducts/index.tsx
@@ -443,7 +443,7 @@ export class DepartmentWiseProducts extends React.Component<any, any> {
                                       </div>
                                       <div className='tbody'>
                                         <div className='td'>
-                                          Admission
+                                          <strong>Admission <i className='fa fa-chevron-up'></i></strong>
                                         </div>
                                         <div className='table performance-table'>
                                           <div className='tbody'>
@@ -455,7 +455,7 @@ export class DepartmentWiseProducts extends React.Component<any, any> {
                                             <div className='td'>User exp</div>
                                           </div>
                                           <div className='tbody'>
-                                            <div className='td'>List &#38; Act</div>
+                                            <div className='td'><span>List &#38; Act</span></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
@@ -463,7 +463,7 @@ export class DepartmentWiseProducts extends React.Component<any, any> {
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                           </div>
                                           <div className='tbody'>
-                                            <div className='td'>Admission</div>
+                                            <div className='td'><span>Admission</span></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle orange'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
@@ -474,7 +474,7 @@ export class DepartmentWiseProducts extends React.Component<any, any> {
                                             <div className='td'><strong>Data Services</strong></div>
                                           </div>
                                           <div className='tbody'>
-                                            <div className='td'>Postgre SQL</div>
+                                            <div className='td'><span>Postgre SQL</span></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle yellow'><i className='fa fa-check-circle'></i></div></div>
@@ -482,13 +482,28 @@ export class DepartmentWiseProducts extends React.Component<any, any> {
                                             <div className='td'><div className='progress-circle red'><i className='fa fa-check-circle'></i></div></div>
                                           </div>
                                           <div className='tbody'>
-                                            <div className='td'>Elastic Search</div>
+                                            <div className='td'><span>Elastic Search</span></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                             <div className='td'><div className='progress-circle green'><i className='fa fa-check-circle'></i></div></div>
                                           </div>
+                                        </div>
+                                      </div>
+                                      <div className='tbody'>
+                                        <div className='td'>
+                                          <strong>Fee <i className='fa fa-chevron-down'></i></strong>
+                                        </div>
+                                      </div>
+                                      <div className='tbody'>
+                                        <div className='td'>
+                                          <strong>Attendance <i className='fa fa-chevron-down'></i></strong>
+                                        </div>
+                                      </div>
+                                      <div className='tbody'>
+                                        <div className='td'>
+                                          <strong>List and Act <i className='fa fa-chevron-down'></i></strong>
                                         </div>
                                       </div>
                                     </div>
@@ -590,6 +605,45 @@ export class DepartmentWiseProducts extends React.Component<any, any> {
                   </div>
                   <div className="tbody">
                     <div className="name"><span>Product 2</span> <i className='fa fa-chevron-down'></i></div>
+                    <div className="app-services">10</div>
+                    <div className="data-services">5</div>
+                    <div className="ou">Admin, Accounts Adminssion, Transport</div>
+                    <div className="edit">
+                      <button className="edit-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </button>
+                    </div>
+                  </div>
+                  <div className="tbody">
+                    <div className="name"><span>Product 3</span> <i className='fa fa-chevron-down'></i></div>
+                    <div className="app-services">10</div>
+                    <div className="data-services">5</div>
+                    <div className="ou">Admin, Accounts Adminssion, Transport</div>
+                    <div className="edit">
+                      <button className="edit-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </button>
+                    </div>
+                  </div>
+                  <div className="tbody">
+                    <div className="name"><span>Product 4</span> <i className='fa fa-chevron-down'></i></div>
+                    <div className="app-services">10</div>
+                    <div className="data-services">5</div>
+                    <div className="ou">Admin, Accounts Adminssion, Transport</div>
+                    <div className="edit">
+                      <button className="edit-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </button>
+                    </div>
+                  </div>
+                  <div className="tbody">
+                    <div className="name"><span>Product 5</span> <i className='fa fa-chevron-down'></i></div>
                     <div className="app-services">10</div>
                     <div className="data-services">5</div>
                     <div className="ou">Admin, Accounts Adminssion, Transport</div>
