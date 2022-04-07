bc9eb43dd7a08b78c8d518fced2603432e671e58
Dipti Patel <diptippatel96@gmail.com>
2022-04-07 10:51:56 +0530

    over view changes

--- public/app/features/overview/index.tsx
+++ public/app/features/overview/index.tsx
@@ -6,31 +6,31 @@ import { AlertVolumeByStatusChart } from './AlertVolumeByStatusChart';
 import { AlertVolumeAnalysis } from './AlertVolumeAnalysis';
 import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 import 'react-multi-carousel/lib/styles.css';
-import Carousel from 'react-multi-carousel';
+// import Carousel from 'react-multi-carousel';
 import { config } from '../config';
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-    slidesToSlide: 1,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 1,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 1,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1,
-  },
-};
+// const responsive = {
+//   superLargeDesktop: {
+//     breakpoint: { max: 4000, min: 3000 },
+//     items: 5,
+//     slidesToSlide: 1,
+//   },
+//   desktop: {
+//     breakpoint: { max: 3000, min: 1024 },
+//     items: 3,
+//     slidesToSlide: 1,
+//   },
+//   tablet: {
+//     breakpoint: { max: 1024, min: 464 },
+//     items: 2,
+//     slidesToSlide: 1,
+//   },
+//   mobile: {
+//     breakpoint: { max: 464, min: 0 },
+//     items: 1,
+//     slidesToSlide: 1,
+//   },
+// };
 
 class Overview extends React.Component<any, any> {
   breadCrumbs: any = [
@@ -84,11 +84,13 @@ class Overview extends React.Component<any, any> {
         totalSpent: {
           value: '$6,71,456',
           diff: '+4 from last week',
+          status: 'down',
           data: [20, 40, 30, 60]
         },
         lastWeekSpent: {
           value: '412,531',
           diff: '+5%',
+          status: 'up',
           data: [40, 20, 60]
         },
         currentSpentRate: {
@@ -234,9 +236,10 @@ class Overview extends React.Component<any, any> {
   };
 
   render() {
+    debugger
     const breadCrumbs = this.breadCrumbs;
     const pageTitle = 'MONITOR | OVERVIEW';
-    const { dailyAvgWaitTime, organizationName, organizationModal, isSubmitted, isLoading } = this.state;
+    const { dailyAvgWaitTime, organizationName, organizationModal, isSubmitted, isLoading, dashboardData } = this.state;
     return (
       <React.Fragment>
         <div className="breadcrumbs-container">
@@ -282,9 +285,92 @@ class Overview extends React.Component<any, any> {
                 </div>
               </div>
             </div>
-            <div className="applications-inner-container">
-
-            </div>
+            {dashboardData &&
+              <>
+                <div className="applications-inner-container">
+                  <div className="row">
+                    <div className="col-xl-9 col-lg-9 col-md-6 col-sm-6">
+                      <div className="dashboard-spent">
+                        <label>Total Spend</label>
+                        <p>{dashboardData.totalSpent.value} <span>{dashboardData.totalSpent.diff}</span></p>
+                      </div>
+                      <div className="dashboard-spent">
+                        <label>Last 7 days Spend</label>
+                        <p>{dashboardData.lastWeekSpent.value} <span>{dashboardData.lastWeekSpent.diff}</span></p>
+                      </div>
+                      <div className="dashboard-spent">
+                        <label>Current spend Rate</label>
+                        <p>{dashboardData.currentSpentRate.perHR} | {dashboardData.currentSpentRate.perDay}</p>
+                      </div>
+                      <div className="dashboard-spent">
+                        <label>Total Budget(Q1)</label>
+                        <p>{dashboardData.totalBudget.total}</p>
+                        <span>{dashboardData.totalBudget.remaining}</span>
+                        <p>{dashboardData.totalBudget.percentage}</p>
+                      </div>
+                      <div className="dashboard-spent">
+                        <label>Spends today</label>
+                        <p>{dashboardData.spentToday.value}  <span>{dashboardData.spentToday.diff}</span></p>
+                      </div>
+                      <div className="dashboard-spent">
+                        <label>Spends yesterday</label>
+                        <p>{dashboardData.spentYesterDay.value} <span>{dashboardData.spentYesterDay.diff}</span></p>
+                      </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3">
+                      <label>Cloud wise spend</label>
+                        <span>...</span>
+                      {dashboardData.cloudWiseProduct &&
+                       dashboardData.cloudWiseProduct.length>0 
+                       &&dashboardData.cloudWiseProduct.map((val:any, index:any) => {
+return(<div key={index}>
+<div>
+  <span><img alt={val.name} src=''></img></span>
+  <div>
+  <span>{val.name}</span> 
+  <span>{val.value}</span>
+  <span>{val.diff}</span>
+  </div>
+  <div>progress bar</div>
+</div>
+</div>)
+                      })
+                      }
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th> Products </th>
+                        <th> Performance </th>
+                        <th> Availibility </th>
+                        <th> Reliability </th>
+                        <th> Security </th>
+                        <th> End Usage </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dashboardData.productList && dashboardData.productList.length > 0 &&
+                        dashboardData.productList.map((val: any, index: any) => {
+                          return (
+                            <tr key={index}>
+                              <td> {val.name} </td>
+                              <td> {val.performance} </td>
+                              <td> {val.availabilty} </td>
+                              <td> {val.reliability} </td>
+                              <td> {val.security} </td>
+                              <td> {val.endUsage} </td>
+                            </tr>
+                          )
+                        })
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              </>
+            }
             {/* <div className="applications-inner-container">
               <Carousel
                 responsive={responsive}
