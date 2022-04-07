679ca12bd6d8fcdba93dad6045f26025320b0aff
Satish Pal <satishweb93@gmail.com>
2022-04-07 15:24:54 +0530

    chart css changes

--- xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/DepartmentWiseProducts/departmentChart.tsx
+++ xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/DepartmentWiseProducts/departmentChart.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Breadcrumbs } from '../Breadcrumbs';
-// import { images } from '../../img';
-// import { PLUGIN_BASE_URL } from '../../constants';
+import { PLUGIN_BASE_URL } from '../../constants';
 import { Bar, Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement } from 'chart.js';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement);
@@ -14,29 +13,25 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
         this.state = {
             displayBarChart: true,
             humanResources: {
-                labels: ['', '', '', ''],
+                labels: ['Production', 'Development', 'Stage', 'Test'],
                 datasets: [
                     {
-                        label: ['Production', 'Development', 'Stage', 'Test'],
-                        lineTension: 0.2,
-                        fill: false,
                         data: [80, 16, 4, 10],
+                        fill: false,
+                        lineTension: 0.2,
                         backgroundColor: [
                             'rgba(82, 177, 65, 1)',
                             'rgba(255, 153, 0, 1)',
                             'rgba(0, 137, 214, 1)',
                             'rgba(216, 69, 57, 1)',
                         ],
-                        borderWidth: 1,
-                        type: 'bar',
                     },
                 ],
             },
             procurment: {
-                labels: ['', '', '', ''],
+                label: ['Production', 'Development', 'Stage', 'Test'],
                 datasets: [
                     {
-                        label: ['Production', 'Development', 'Stage', 'Test'],
                         lineTension: 0.2,
                         fill: false,
                         data: [80, 16, 4, 10],
@@ -52,10 +47,9 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                 ],
             },
             supplyChain: {
-                labels: ['', '', '', ''],
+                label: ['Production', 'Development', 'Stage', 'Test'],
                 datasets: [
                     {
-                        label: ['Production', 'Development', 'Stage', 'Test'],
                         lineTension: 0.2,
                         fill: false,
                         data: [80, 16, 4, 10],
@@ -71,10 +65,9 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                 ],
             },
             EMS: {
-                labels: ['', '', '', ''],
+                label: ['Production', 'Development', 'Stage', 'Test'],
                 datasets: [
                     {
-                        label: ['Production', 'Development', 'Stage', 'Test'],
                         lineTension: 0.2,
                         fill: false,
                         data: [80, 16, 4, 10],
@@ -145,6 +138,81 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                     }
                 ],
             },
+            barPptions: {
+                plugins: {
+                    scales: {
+                        y: {
+                            ticks: {
+                                fontColor: 'black',
+                                stepSize: 10,
+                                beginAtZero: true,
+                            },
+                            gridLines: {
+                                display: false
+                            }
+                        },
+                        x: {
+                            ticks: {
+                                fontColor: 'black',
+                                display: false,
+                                stepSize: 10,
+                            },
+                            gridLines: {
+                                display: false
+                            }
+                        },
+                    },
+                    legend: {
+                        labels: {
+                            usePointStyle: true,
+                            pointStyle: 'circle',
+                        },
+                        display: true,
+                        position:'right',
+                        top: 12,
+                        bottom: 12,
+                        
+                    },
+                    responsive: true,
+                }
+            },
+            piePptions: {
+                plugins: {
+                    scales: {
+                        y: {
+                            ticks: {
+                                fontColor: 'black',
+                                stepSize: 10,
+                                beginAtZero: true,
+                            },
+                            gridLines: {
+                                display: false
+                            }
+                        },
+                        x: {
+                            ticks: {
+                                fontColor: 'black',
+                                display: false,
+                                stepSize: 10,
+                            },
+                            gridLines: {
+                                display: false
+                            }
+                        },
+                    },
+                    legend: {
+                        labels: {
+                            usePointStyle: true,
+                            pointStyle: 'circle',
+                        },
+                        display: true,
+                        position:'right',
+                        top: 12,
+                        bottom: 12,
+                    },
+                    responsive: true,
+                }
+            }
         };
         this.breadCrumbs = [
             {
@@ -157,54 +225,34 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
             },
         ];
     }
-
-    options = {
-        scales: {
-            y: {
-                ticks: {
-                    fontColor: 'black',
-                    stepSize: 10,
-                    beginAtZero: true,
-                },
-                gridLines: {
-                    display: false
-                }
-            },
-            x: {
-                ticks: {
-                    fontColor: 'black',
-                    display: false,
-                    stepSize: 10,
-                },
-                gridLines: {
-                    display: false
-                }
-            },
-        },
-        legend: {
-            display: true,
-            align: "right",
-            position:'right'
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-    };
+    
+    
 
     render() {
-        const { displayBarChart } = this.state
+        const { displayBarChart, barPptions, piePptions } = this.state
         return (
             <div className="asset-container">
                 <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="ASSET MANAGEMENT" />
                 {<div className="department-wise-container">
-                    <div className="common-container">
-                        <div className="row">
-                            <div className="col-lg-9 col-md-8 col-sm-6">
-                                <div className="asset-heading">Product Wise Cost</div>
-                            </div>
-                            <div className="col-lg-3 col-md-4 col-sm-6">
-                                <div className="float-right common-right-btn">
-                                    <input type="checkbox" checked={false}
-                                        onClick={() => { this.setState({ displayBarChart: !displayBarChart }) }} />{!displayBarChart ? "pieChart" : "Bar Graph"}
+                    <div className="common-container border-bottom-0">
+                        <div className="department-heading">
+                            <div className="row">
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="asset-heading">Product Wise Cost</div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="float-right common-right-btn">
+                                        <button
+                                            className="asset-white-button min-width-inherit"
+                                            onClick={() => {this.setState({ displayBarChart: !displayBarChart })}} 
+                                        >
+                                            <i className="fa fa-square-o"></i>&nbsp;&nbsp;
+                                            {!displayBarChart ? "Pie Chart" : "Bar Graph"}
+                                        </button>
+                                        <Link to={`${PLUGIN_BASE_URL}/department-wise-products`} className="asset-white-button min-width-inherit">
+                                            <i className="fa fa-arrow-circle-left"></i>&nbsp;&nbsp; Back
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -215,7 +263,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">Human Resources</div>
                                         <div className="total-cost-text cost"><strong>Total cost: $415624</strong> - 40% off the total cost</div>
                                         <div className="chart">
-                                            <Bar data={{ datasets: this.state.humanResources.datasets, labels: this.state.humanResources.labels }} options={this.options} />
+                                            <Bar data={this.state.humanResources} options={barPptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -224,7 +272,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">Procurement</div>
                                         <div className="total-cost-text cost"><strong>Total Cost: $73837</strong> - 40% of the total cost</div>
                                         <div className="chart">
-                                            <Bar data={{ datasets: this.state.procurment.datasets, labels: this.state.procurment.labels }} options={this.options} />
+                                            <Bar data={this.state.procurment} options={barPptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -235,7 +283,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">Supply Chain Management</div>
                                         <div className="total-cost-text cost"><strong>Total Cost: $73837</strong> - 40% off the total cost</div>
                                         <div className="chart">
-                                            <Bar data={{ datasets: this.state.supplyChain.datasets, labels: this.state.supplyChain.labels }} options={this.options} />
+                                            <Bar data={{ datasets: this.state.supplyChain.datasets, labels: this.state.supplyChain.labels }} options={barPptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -244,7 +292,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">EMS</div>
                                         <div className="total-cost-text cost"><strong>Total Cost: $73837</strong> - 40% off the total cost</div>
                                         <div className="chart">
-                                            <Bar data={{ datasets: this.state.EMS.datasets, labels: this.state.EMS.labels }} options={this.options} />
+                                            <Bar data={{ datasets: this.state.EMS.datasets, labels: this.state.EMS.labels }} options={barPptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -258,7 +306,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">Human Resources</div>
                                         <div className="total-cost-text cost"><strong>Total cost: $415624</strong> - 40% off the total cost</div>
                                         <div className="chart">
-                                            <Pie data={this.state.humanResourcespieData} />
+                                            <Pie data={this.state.humanResourcespieData} options={piePptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -267,7 +315,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">Procurement</div>
                                         <div className="total-cost-text cost"><strong>Total Cost: $73837</strong> - 40% of the total cost</div>
                                         <div className="chart">
-                                            <Pie data={this.state.procurmentpieData} />
+                                            <Pie data={this.state.procurmentpieData} options={piePptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -278,7 +326,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">Supply Chain Management</div>
                                         <div className="total-cost-text cost"><strong>Total Cost: $73837</strong> - 40% off the total cost</div>
                                         <div className="chart">
-                                            <Pie data={this.state.supplyChainpieData} />
+                                            <Pie data={this.state.supplyChainpieData} options={piePptions} />
                                         </div>
                                     </div>
                                 </div>
@@ -287,7 +335,7 @@ export class DepartmentWiseCharts extends React.Component<any, any> {
                                         <div className="heading">EMS</div>
                                         <div className="total-cost-text cost"><strong>Total Cost: $73837</strong> - 40% off the total cost</div>
                                         <div className="chart">
-                                            <Pie data={this.state.EMSpieData} />
+                                            <Pie data={this.state.EMSpieData} options={piePptions} />
                                         </div>
                                     </div>
                                 </div>
