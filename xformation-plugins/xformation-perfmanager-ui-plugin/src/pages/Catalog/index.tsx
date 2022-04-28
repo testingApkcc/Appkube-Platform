bb3d3661b7832913cb94da39709acef80335d2ba
Satish Pal <satishweb93@gmail.com>
2022-04-28 19:07:35 +0530

    UI for catalogue in perfmanager

--- xformation-plugins/xformation-perfmanager-ui-plugin/src/pages/Catalog/index.tsx
+++ xformation-plugins/xformation-perfmanager-ui-plugin/src/pages/Catalog/index.tsx
@@ -3,6 +3,9 @@ import { Breadcrumbs } from '../Breadcrumbs';
 import { TopMenu } from './TopMenu';
 import previewDashboardIcon from '../../img/preview-dashboard-icon.png';
 import libraryIcon from '../../img/library-icon.png';
+import awsIcon from '../../img/aws.png';
+
+
 // import previewDashboard from '../../img/preview-dashboard.png';
 
 export class Catalog extends React.Component<any, any>{
@@ -328,7 +331,7 @@ export class Catalog extends React.Component<any, any>{
                     </div>
                   </div>
                   <div className="col-lg-9 col-md-9 col-sm-12 col-l-p">
-                    <div className="catalogue-right-container">
+                    {/* <div className="catalogue-right-container">
                       <div className="heading">
                         <div className="row">
                           <div className="col-md-9 col-sm-12">
@@ -344,6 +347,101 @@ export class Catalog extends React.Component<any, any>{
                         {this.handleCard(cardData)}
                       </div> : cardData.length > 0 && <div className="loading-text">...loading</div>
                       }
+                    </div> */}
+                    <div className="catalogue-right-container">
+                      <div>
+                        Select a template to start with. You can use filters or the seach box the scope.
+                      </div>
+                      <div className="templated-search">
+                        <div className="row">
+                          <div className="col-sm-10">
+                            <div className="search-box">
+                              <button className="search-button"><i className="fa fa-search"></i></button>
+                              <input type="text" placeholder="Search Template here" className="input" />
+                            </div>
+                          </div>
+                          <div className="col-sm-2">
+                            <div className="btnContainer">
+                              <button className="btn btn-grid btn-active"><i className="fa fa-th-large"></i></button>
+                              <button className="btn btn-list"><i className="fa fa-list"></i></button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="templated-boxs">
+                        <div className="row">
+                          <div className="col-md-6 col-sm-12">
+                            <div className="template-box">
+                              <div className="heading">
+                                <img src={awsIcon} alt='' />
+                                Landing Zone
+                              </div>
+                              <div className="sub-text">
+                                Create Landing Zone with DevSecOps best practice in AWS
+                              </div>
+                              <div className="text">
+                                Description text related to creating LAmding zone on AWS will be displayed here
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-md-6 col-sm-12">
+                            <div className="template-box">
+                              <div className="heading">
+                                <img src={awsIcon} alt='' />
+                                Landing Zone
+                              </div>
+                              <div className="sub-text">
+                                Create Landing Zone with DevSecOps best practice in AWS
+                              </div>
+                              <div className="text">
+                                Description text related to creating LAmding zone on AWS will be displayed here
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-md-6 col-sm-12">
+                            <div className="template-box">
+                              <div className="heading">
+                                <img src={awsIcon} alt='' />
+                                Landing Zone
+                              </div>
+                              <div className="sub-text">
+                                Create Landing Zone with DevSecOps best practice in AWS
+                              </div>
+                              <div className="text">
+                                Description text related to creating LAmding zone on AWS will be displayed here
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-md-6 col-sm-12">
+                            <div className="template-box">
+                              <div className="heading">
+                                <img src={awsIcon} alt='' />
+                                Landing Zone
+                              </div>
+                              <div className="sub-text">
+                                Create Landing Zone with DevSecOps best practice in AWS
+                              </div>
+                              <div className="text">
+                                Description text related to creating LAmding zone on AWS will be displayed here
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-md-6 col-sm-12">
+                            <div className="template-box">
+                              <div className="heading">
+                                <img src={awsIcon} alt='' />
+                                Landing Zone
+                              </div>
+                              <div className="sub-text">
+                                Create Landing Zone with DevSecOps best practice in AWS
+                              </div>
+                              <div className="text">
+                                Description text related to creating LAmding zone on AWS will be displayed here
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
