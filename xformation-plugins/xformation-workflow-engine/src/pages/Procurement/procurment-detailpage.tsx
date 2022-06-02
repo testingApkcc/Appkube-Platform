c2e1d8fc7bd150e73e52118fa941d717cbeb68ea
git-himanshu-seth <hseth879157@gmail.com>
2022-06-02 15:42:15 +0530

    changes

--- xformation-plugins/xformation-workflow-engine/src/pages/Procurement/procurment-detailpage.tsx
+++ xformation-plugins/xformation-workflow-engine/src/pages/Procurement/procurment-detailpage.tsx
@@ -3,6 +3,7 @@ import { Breadcrumbs } from '../Breadcrumbs';
 import { AssetView } from './assetView';
 import { OverView } from './overView';
 import { WorkFlowView } from './workFlowView';
+import headerIcon from '../../img/header-icon.png';
 import 'react-circular-progressbar/dist/styles.css';
 import 'simplebar/dist/simplebar.min.css';
 
@@ -35,33 +36,49 @@ export class ProcurementDetail extends React.Component<any, any> {
     const { activeTab } = this.state;
     return (
       <div className="owrkflow-project-wise-container">
-        <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="WORKFLOW MANAGEMENT" />
-        <div className="project-wise-page-container row">
-          <div className='col-12'>
-                  <ul className="row">
-          {
-            this.stepper.map(({ title, key}: any, index: any) => {
-              return (
-          
-                    <li key={index} onClick={() => this.activeTab(key)} className={key === activeTab ? "active col-4" : 'col-4'}>{title}</li>
-                
-              )
-            })
-          }
+        <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="Usecase 1" />
+        <div className="project-wise-page-container">
+          <div className="project-wise-page-headin">
+            <div className="row">
+              <div className="col-lg-10">
+                <div className="heading-content-left">
+                  <div className="heading-icon"><img src={headerIcon} alt="" /></div>
+                  <div className="heading-content">
+                    <h3>Xformation Platform</h3>
+                    <span>Last updated by Siddhesh D 24 min ago</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2">
+                <div className="heading-content-right">
+                  <button >Project Overview</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div >
+            <ul className="row">
+              {
+                this.stepper.map(({ title, key }: any, index: any) => {
+                  return (
+                    <li key={index} onClick={() => this.activeTab(key)} className={key === activeTab ? "active col-4 " : 'col-4'}>{title}</li>
+                  )
+                })
+              }
             </ul>
           </div>
-          <div className='col-12'>
-          {
-            this.stepper.map(({  key, component }: any, index: any) => {
-              return (
-              <div key={index}>
-                  {activeTab === key ? 
-                    <>{component}</>
-                 : <></>}
-              </div>
-              )
-            })
-          }</div>
+          <div>
+            {
+              this.stepper.map(({ key, component }: any, index: any) => {
+                return (
+                  <div key={index}>
+                    {activeTab === key ?
+                      <>{component}</>
+                      : <></>}
+                  </div>
+                )
+              })
+            }</div>
         </div>
       </div>
     );
