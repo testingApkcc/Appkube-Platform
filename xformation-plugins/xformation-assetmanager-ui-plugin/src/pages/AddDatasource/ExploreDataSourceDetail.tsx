182be6efe36da7664e17ccfdad577ed8661d86ad
Dipti Patel <diptippatel96@gmail.com>
2022-07-05 11:29:51 +0530

    remove extra code from explore data source

--- xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/AddDatasource/ExploreDataSourceDetail.tsx
+++ xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/AddDatasource/ExploreDataSourceDetail.tsx
@@ -1,30 +1,20 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Breadcrumbs } from '../Breadcrumbs';
 // import { configFun } from '../../config';
 import { images } from '../../img';
-import { CommonService } from '../_common/common';
+// import { CommonService } from '../_common/common';
 // import { SelectCloudFilter } from '../../components/SelectCloudFilter';
 // import { RestService } from '../_service/RestService';
-import { PLUGIN_BASE_URL } from '../../constants';
+// import { PLUGIN_BASE_URL } from '../../constants';
 
 export class ExploreDataSourceDetail extends React.Component<any, any> {
 	breadCrumbs: any;
 	constructor(props: any) {
 		super(props);
 		this.state = {
-			environment: '',
-			account: '',
-			sourceList: [
-				{ name: 'AWS-PullMetric-API', cloud: 'Cloud Watch', description: 'pull aws metrics with cloud api' },
-				{ name: 'AWS-PullLogs-API', cloud: 'Cloud Watch', description: 'pull aws Logs with cloud api' },
-				{
-					name: 'AWS-StoreTrace-Local',
-					cloud: 'AWS',
-					description: 'Receive traces and store in local Zipkin DB'
-				},
-				{ name: 'AWS-PullLogs-Local', cloud: 'AWS', description: 'Receive AWS Logs and store in Local ES' }
-			]
+			queryName: '',
+			account: ''
 		};
 		this.breadCrumbs = [
 			{
@@ -42,50 +32,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 		// await this.getAccountList();
 	}
 
-	getAccountList = async () => {
-		// try {
-		// 	await RestService.getData(this.config.GET_ALL_ACCOUNT, null, null).then((response: any) => {
-		// 		this.setState({
-		// 			accountList: response
-		// 		});
-		// 		console.log('Loading Asstes : ', response);
-		// 	});
-		// } catch (err) {
-		// 	console.log('Loading Asstes failed. Error: ', err);
-		// }
-	};
-
-	displayDataSource = () => {
-		let retData = [];
-		const { sourceList } = this.state;
-		let accountId = CommonService.getParameterByName('accountId', window.location.href);
-		if (sourceList && sourceList.length > 0) {
-			for (let i = 0; i < sourceList.length; i++) {
-				retData.push(
-					<div className="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<Link
-							to={`${PLUGIN_BASE_URL}/add-datasource-credential?sourceName=${sourceList[i]
-								.name}&&accountId=${accountId}`}
-						>
-							<div className="source-box">
-								<div className="images">
-									<img src={images.awsLogo} alt="" />
-								</div>
-								<div className="Source-content">
-									<label>{sourceList[i].name}</label>
-									<span>{sourceList[i].cloud}</span>
-									<p>{sourceList[i].description}</p>
-								</div>
-							</div>
-						</Link>
-					</div>
-				);
-			}
-		}
-		return retData;
-	};
-
-	onChangeDataSource = (e: any) => {
+	handleStateChange = (e: any) => {
 		const { name, value } = e.target;
 		this.setState({
 			[name]: value
@@ -146,8 +93,11 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																<div className="explore-head-right">
 																	<ul>
 																		<li>
-																			<button type="button" className="btn btn-light split-btn">
-																				<i className="fal fa-pause"></i>
+																			<button
+																				type="button"
+																				className="btn btn-light split-btn"
+																			>
+																				<i className="fal fa-pause" />
 																				Split
 																			</button>
 																		</li>
@@ -157,33 +107,53 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																					className="input-group-text"
 																					name="environment"
 																					value=""
-																					onChange={this.onChangeDataSource}
+																					onChange={this.handleStateChange}
 																				>
-																					<option key="1" value="Last 1 hours">
+																					<option
+																						key="1"
+																						value="Last 1 hours"
+																					>
 																						Last 1 hours
 																					</option>
-																					<option key="2" value="Last 2 hours">
+																					<option
+																						key="2"
+																						value="Last 2 hours"
+																					>
 																						Last 2 hours
 																					</option>
-																					<option key="3" value="Last 3 hours">
+																					<option
+																						key="3"
+																						value="Last 3 hours"
+																					>
 																						Last 3 hours
 																					</option>
-																					<option key="4" value="Last 4 hours">
+																					<option
+																						key="4"
+																						value="Last 4 hours"
+																					>
 																						Last 4 hours
 																					</option>
-																					<option key="5" value="Last 5 hours">
+																					<option
+																						key="5"
+																						value="Last 5 hours"
+																					>
 																						Last 5 hours
 																					</option>
 																				</select>
-																				<i className="far fa-clock"></i>
+																				<i className="far fa-clock" />
 																			</div>
 																		</li>
 																		<li>
-																			<button className="asset-blue-button">Run Query</button>
+																			<button className="asset-blue-button">
+																				Run Query
+																			</button>
 																		</li>
 																		<li>
-																			<button type="button" className="btn btn-link list-btn">
-																				<i className="fas fa-bars"></i>
+																			<button
+																				type="button"
+																				className="btn btn-link list-btn"
+																			>
+																				<i className="fas fa-bars" />
 																			</button>
 																		</li>
 																	</ul>
@@ -202,7 +172,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																		className="input-group-text"
 																		name="environment"
 																		value=""
-																		onChange={this.onChangeDataSource}
+																		onChange={this.handleStateChange}
 																	>
 																		<option key="1" value="Default">
 																			Default
@@ -222,7 +192,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																		className="input-group-text"
 																		name="environment"
 																		value=""
-																		onChange={this.onChangeDataSource}
+																		onChange={this.handleStateChange}
 																	>
 																		<option key="1" value="Default">
 																			CloudWatch Metrics
@@ -242,7 +212,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																		className="input-group-text"
 																		name="environment"
 																		value=""
-																		onChange={this.onChangeDataSource}
+																		onChange={this.handleStateChange}
 																	>
 																		<option key="1" value="Default">
 																			Metrics Search
@@ -267,7 +237,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																		className="input-group-text"
 																		name="environment"
 																		value=""
-																		onChange={this.onChangeDataSource}
+																		onChange={this.handleStateChange}
 																	>
 																		<option key="1" value="aws">
 																			Select
@@ -285,7 +255,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																		className="input-group-text"
 																		name="environment"
 																		value=""
-																		onChange={this.onChangeDataSource}
+																		onChange={this.handleStateChange}
 																	>
 																		<option key="1" value="aws">
 																			Select
@@ -303,7 +273,7 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 																		className="input-group-text"
 																		name="environment"
 																		value=""
-																		onChange={this.onChangeDataSource}
+																		onChange={this.handleStateChange}
 																	>
 																		<option key="1" value="aws">
 																			Select
@@ -318,16 +288,24 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 													</div>
 													<div className="dimension-heading">
 														<label>Dimension</label>
-														<button type="button" className="btn btn-light dimension-add-btn">
-															<i className="fas fa-plus"></i>
+														<button
+															type="button"
+															className="btn btn-light dimension-add-btn"
+														>
+															<i className="fas fa-plus" />
 														</button>
 													</div>
 													<div className="dimension-heading">
-														<label>Match exact &#8722; <span>optional</span><i className="fal fa-exclamation-circle"></i></label>
+														<label>
+															Match exact &#8722; <span>optional</span>
+															<i className="fal fa-exclamation-circle" />
+														</label>
 														<div className="checkbox switcher">
 															<label>
 																<input type="checkbox" id="test1" value="" checked />
-																<span><small></small></span>
+																<span>
+																	<small />
+																</span>
 															</label>
 														</div>
 													</div>
@@ -335,19 +313,32 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 														<div className="row">
 															<div className="col-xl-4 col-lg-4 col-md-4 col-smm-12 col-xs-12">
 																<div className="form-group description-content">
-																	<label htmlFor="description">ID &#8722; <span>optional</span><i className="fal fa-exclamation-circle"></i></label>
+																	<label htmlFor="description">
+																		ID &#8722; <span>optional</span>
+																		<i className="fal fa-exclamation-circle" />
+																	</label>
 																	<input type="text" className="form-control" />
 																</div>
 															</div>
 															<div className="col-xl-4 col-lg-4 col-md-4 col-smm-12 col-xs-12">
 																<div className="form-group description-content">
-																	<label htmlFor="description">Period<span></span><i className="fal fa-exclamation-circle"></i></label>
-																	<input type="text" className="form-control" placeholder="Auto" />
+																	<label htmlFor="description">
+																		Period<span />
+																		<i className="fal fa-exclamation-circle" />
+																	</label>
+																	<input
+																		type="text"
+																		className="form-control"
+																		placeholder="Auto"
+																	/>
 																</div>
 															</div>
 															<div className="col-xl-4 col-lg-4 col-md-4 col-smm-12 col-xs-12">
 																<div className="form-group description-content">
-																	<label htmlFor="description">Alias &#8722; <span>optional</span><i className="fal fa-exclamation-circle"></i></label>
+																	<label htmlFor="description">
+																		Alias &#8722; <span>optional</span>
+																		<i className="fal fa-exclamation-circle" />
+																	</label>
 																	<input type="text" className="form-control" />
 																</div>
 															</div>
@@ -355,9 +346,15 @@ export class ExploreDataSourceDetail extends React.Component<any, any> {
 													</div>
 												</div>
 												<div className="explore-buttons">
-													<button className="asset-blue-button"><i className="fas fa-plus"></i> Add query</button>
-													<button className="asset-blue-button"><i className="far fa-clock"></i>Query history</button>
-													<button className="asset-blue-button"><i className="fal fa-exclamation-circle"></i>inspector</button>
+													<button className="asset-blue-button">
+														<i className="fas fa-plus" /> Add query
+													</button>
+													<button className="asset-blue-button">
+														<i className="far fa-clock" />Query history
+													</button>
+													<button className="asset-blue-button">
+														<i className="fal fa-exclamation-circle" />inspector
+													</button>
 												</div>
 											</div>
 										</div>
