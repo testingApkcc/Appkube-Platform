ed66636211b2020f41cce807af35d67b2dedbb56
Sunil Baghel <suneelsingh038@gmail.com>
2022-07-06 14:38:07 +0530

    ui adddatasource page changes

--- xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/AddDatasource/index.tsx
+++ xformation-plugins/xformation-assetmanager-ui-plugin/src/pages/AddDatasource/index.tsx
@@ -112,7 +112,7 @@ export class AddDatasource extends React.Component<any, any> {
 		}
 		if (retData && retData.length == 0) {
 			retData.push(
-				<div className="">
+				<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
 					<span>Data source not found, select other account.</span>
 				</div>
 			);
