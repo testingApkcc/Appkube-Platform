138bd163d0bc1c386e9aa08bbe022b77eeb5698b
manojkrjoshi <manojkr.joshi@gmail.in>
2022-11-03 10:12:41 +0530

    test gelf log api

--- pkg/services/notifications/gelf_tcp.go
+++ pkg/services/notifications/gelf_tcp.go
@@ -2,8 +2,6 @@ package notifications
 
 import (
 	"context"
-
-	"gopkg.in/Graylog2/go-gelf.v2/gelf"
 )
 
 type GelfTcp struct {
@@ -14,14 +12,14 @@ type GelfTcp struct {
 
 func (ns *NotificationService) sendGelfTcpRequestSync(ctx context.Context, gelftcp *GelfTcp) error {
 	ns.log.Debug("Sending gelf message", gelftcp.GelfMessage, "gelf server", gelftcp.GelfServer, "gelf server port", gelftcp.GelfTcpPort)
-	gelfTcpURL := gelftcp.GelfServer + ":" + gelftcp.GelfTcpPort
-	_, err := gelf.NewTCPWriter(gelfTcpURL)
+	// gelfTcpURL := gelftcp.GelfServer + ":" + gelftcp.GelfTcpPort
+	// gelfWriter, err := gelf.NewTCPWriter(gelfTcpURL)
 
-	if err != nil {
-		ns.log.Error("Cannot connect to GELF server: ", err)
-		return err
-	}
-	//log.SetOutput(io.MultiWriter(os.Stderr, gelfWriter))
+	// if err != nil {
+	// 	ns.log.Error("Cannot connect to GELF server: ", err)
+	// 	return err
+	// }
+	// log.SetOutput(io.MultiWriter(os.Stderr, gelfWriter))
 	//log.Printf(string(gelftcp.GelfMessage))
 	return nil
 }
