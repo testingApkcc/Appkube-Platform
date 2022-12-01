f6214c973eca35dbfd748eb13beb71173f4b886c
manojkrjoshi <manojkr.joshi@gmail.in>
2022-12-01 23:53:51 +0530

    code changes to test alert message

--- pkg/services/notifications/gelf_tcp.go
+++ pkg/services/notifications/gelf_tcp.go
@@ -1,12 +1,13 @@
 package notifications
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/grafana/grafana/pkg/models"
 )
@@ -16,6 +17,14 @@ import (
 // 	GelfTcpPort string
 // 	GelfMessage string
 // }
+
+type Msg struct {
+	version       string
+	host          string
+	short_message string
+	level         int64
+	_some_info    string
+}
 
 func (ns *NotificationService) sendGelfTcpRequestSync(ctx context.Context, gelftcp *models.SendGelfTcpSync) error {
 	ns.log.Debug("Sending gelf message", gelftcp.GelfMessage, "gelf server", gelftcp.GelfServer, "gelf server port", gelftcp.GelfTcpPort)
@@ -32,10 +41,20 @@ func (ns *NotificationService) sendGelfTcpRequestSync(ctx context.Context, gelft
 	httpUrl := "http://" + gelftcp.GelfServer + ":" + gelftcp.GelfTcpPort + "/gelf"
 	method := "POST"
 
-	payload := strings.NewReader(`{` + "" + `    "version": "1.1",` + "" + `    "host": "182.69.97.205",` + "" + `    "short_message": " ` + gelftcp.GelfMessage + ` ",` + "" + `    "level": 5,` + "" + `    "_some_info": "Fist gelf http message"` + "" + `}`)
+	m := Msg{
+		version:       "1.1",
+		host:          "3.211.7.242",
+		short_message: gelftcp.GelfMessage,
+		level:         5,
+		_some_info:    "Gelf Alert",
+	}
+
+	// payload := strings.NewReader(`{` + "" + `    "version": "1.1",` + "" + `    "host": "182.69.97.205",` + "" + `    "short_message": " ` + gelftcp.GelfMessage + ` ",` + "" + `    "level": 5,` + "" + `    "_some_info": "Fist gelf http message"` + "" + `}`)
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(m)
 
 	client := &http.Client{}
-	req, err := http.NewRequest(method, httpUrl, payload)
+	req, err := http.NewRequest(method, httpUrl, &buf)
 
 	if err != nil {
 		fmt.Println(err)
