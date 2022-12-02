4769871aee0e65a53f77b7d82927eccae8f8f8b2
manojkrjoshi <manojkr.joshi@gmail.in>
2022-12-02 12:12:28 +0530

    code changes to test alert message

--- pkg/services/notifications/gelf_tcp.go
+++ pkg/services/notifications/gelf_tcp.go
@@ -2,8 +2,10 @@ package notifications
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
@@ -16,47 +18,38 @@ import (
 // 	GelfMessage string
 // }
 
-type Msg struct {
-	version       string
-	host          string
-	short_message string
-	level         int64
-	_some_info    string
+type GraylogGelfHttpMessage struct {
+	Version string `json:"version"`
+	// Host          string `json:"host"` // Its not mandatory field for graylog gelf http message
+	Short_message string `json:"short_message"`
+	Level         int    `json:"level"`
+	Some_info     string `json:"_some_info"`
 }
 
 func (ns *NotificationService) sendGelfTcpRequestSync(ctx context.Context, gelftcp *models.SendGelfTcpSync) error {
 	ns.log.Debug("Sending gelf message", gelftcp.GelfMessage, "gelf server", gelftcp.GelfServer, "gelf server port", gelftcp.GelfTcpPort)
-	// gelfTcpURL := gelftcp.GelfServer + ":" + gelftcp.GelfTcpPort
-	// gelfWriter, err := gelf.NewTCPWriter(gelfTcpURL)
 
-	// if err != nil {
-	// 	ns.log.Error("Cannot connect to GELF server: ", err)
-	// 	return err
-	// }
-	// log.SetOutput(io.MultiWriter(os.Stderr, gelfWriter))
-	// log.Printf("ALERT :::: " + string(gelftcp.GelfMessage))
+	// log.Printf("Original ALERT :::: " + string(gelftcp.GelfMessage))
 
 	httpUrl := "http://" + gelftcp.GelfServer + ":" + gelftcp.GelfTcpPort + "/gelf"
 	method := "POST"
 
-	// m := Msg{
-	// 	version:       "1.1",
-	// 	host:          "3.211.7.242",
-	// 	short_message: "{'big':'one'}",
-	// 	level:         5,
-	// 	_some_info:    "Gelf Alert",
-	// }
+	m := &GraylogGelfHttpMessage{
+		Version: "1.1",
+		// Host:          "3.211.7.242", // Its not mandatory field for graylog gelf http message
+		Short_message: gelftcp.GelfMessage,
+		Level:         5,
+		Some_info:     "Gelf Alert",
+	}
+	msgJson, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	strMsg := string(msgJson)
+	fmt.Println("Graylog msg: " + strMsg)
 
-	payload := strings.NewReader(`{` + "" + `    "version": "1.1",` + "" + `    "host": "182.69.97.205",` + "" + `    "short_message": " helo ",` + "" + `    "level": 5,` + "" + `    "_some_info": "Fist gelf http message"` + "" + `}`)
-
-	// requestByte, err := json.Marshal(m)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// 	return err
-	// }
-
-	// log.Printf("MSG::::: ", payload)
-	// reader := bytes.NewReader(payload)
+	payload := strings.NewReader(string(msgJson))
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, httpUrl, payload)
