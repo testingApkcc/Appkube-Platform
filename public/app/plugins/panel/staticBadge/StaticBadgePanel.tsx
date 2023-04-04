5f3e3c6b0a9abb0cc209cc691c808cd4581a99a2
Satish Pal <satishweb93@gmail.com>
2023-04-04 17:39:36 +0530

    added StaticBadgePanel with fake api endpoint fetch

--- public/app/plugins/panel/staticBadge/StaticBadgePanel.tsx
+++ public/app/plugins/panel/staticBadge/StaticBadgePanel.tsx
@@ -1,22 +1,79 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { PanelProps } from '@grafana/data';
 import './css/style.css';
+import { StaticBadgeOptions } from './types';
 
-type Props = {};
+type MyState = {
+  data: {
+    cost: string;
+  };
+};
 
-const StaticBadgePanel = (props: Props) => {
-  return (
-    <div className="static-badge-panel-main">
-      <div className="static-badge-panel">
-        <div className="static-badge-circle">
-          <img src={'/public/img/static_badge_icon.png'} alt="Static badge" />
-        </div>
-        <div className="static-badge-contents">
-          <div className="text">Total Functions</div>
-          <div className="price">540,549</div>
+class StaticBadgePanel extends PureComponent<PanelProps<StaticBadgeOptions>> {
+  state: MyState = {
+    data: {
+      cost: '',
+    },
+  };
+
+  componentDidMount = () => {
+    if (this.props.options.gaugeURL) {
+      fetch(this.props.options.gaugeURL)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Something went wrong');
+        })
+        .then((responseJson) => {
+          this.setState({ data: responseJson });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  async componentDidUpdate(prevProps: any, prevState: any) {
+    if (prevProps.options.gaugeURL !== this.props.options.gaugeURL) {
+      await fetch(this.props.options.gaugeURL)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Something went wrong');
+        })
+        .then((responseJson) => {
+          this.setState({ data: responseJson });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="static-badge-panel-main">
+        <div className="static-badge-panel">
+          {this.props.options.gaugeURL && this.state.data.cost ? (
+            <>
+              <div className="static-badge-circle">
+                <img src={`data:image/png;base64,${this.props.options.gaugeIMG}`} alt="Static badge" />
+              </div>
+              <div className="static-badge-contents">
+                <div className="text">{this.props.options.gaugeTitle}</div>
+                <div className="price">{data.cost}</div>
+              </div>
+            </>
+          ) : (
+            <p style={{ textAlign: 'center' }}>Please enter API endpoint to fetch data</p>
+          )}
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default StaticBadgePanel;
