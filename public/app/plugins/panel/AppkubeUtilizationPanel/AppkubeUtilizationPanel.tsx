effaf2743f17afcebc11abf4b63670aaeea58062
Jasmin Makasana <jasminmakasana1991@gmail.com>
2024-01-19 16:35:56 +0530

    Update: dummy data removed from UtilizationPanel

--- public/app/plugins/panel/AppkubeUtilizationPanel/AppkubeUtilizationPanel.tsx
+++ public/app/plugins/panel/AppkubeUtilizationPanel/AppkubeUtilizationPanel.tsx
@@ -1,58 +1,83 @@
 import React, { PureComponent } from 'react';
 import { PanelProps } from '@grafana/data';
 import './css/style.css';
-import data from './response.json'
-
-type MyState = {
-  data: {
-    tableData: any[];
-  };
-  queryParams: {
-    from: string;
-    to: string;
-  };
-};
 
 class AppkubeUtlizationPanel extends PureComponent<PanelProps> {
-  state: MyState = {
-    data: {
-      tableData: [],
-    },
-    queryParams: {
-      from: 'now-6h',
-      to: 'now',
-    },
-  };
-
-  renderData = () => {
-    console.log(data);
-    const JSX: any[] = [];
-    Object.entries(data).map((value) => {
-      JSX.push( 
-      <div className="info">
-      <span className="name">{value[0].split(/(?=[A-Z])/).join(" ")}</span>
-      <span className="percantage">{value[1].MetricDataResults[0].Values[0]}%</span>
-    </div>)
-   });
-   return JSX;
-  }
-
-  render() {
+  renderCPUUtilizationData = (data: any) => {
     return (
-      <div className="appkube-utilization-panel">
       <div className="utilization-card">
         <div className="card-title">
           <div className="icon">
-            <img src="" alt=""/>
+            <img src="" alt="" />
           </div>
           <span className="name">CPU Utilization</span>
         </div>
         <div className="utilization-details">
-          {this.renderData()}
+          <div className="info">
+            <span className="name">Current Usage</span>
+            <span className="percantage">{data.CurrentUsage}%</span>
+          </div>
+          <div className="info">
+            <span className="name">Average Usage</span>
+            <span className="percantage">{data.AverageUsage}%</span>
+          </div>
+          <div className="info">
+            <span className="name">Max Usage</span>
+            <span className="percantage">{data.MaxUsage}%</span>
+          </div>
         </div>
       </div>
-    </div>
     );
+  };
+
+  renderFrames = (series: any) => {
+    const retData: any = [];
+    for (let i = 0; i < series.length; i++) {
+      const iSer = series[i];
+      let cardJSX: any = [];
+      if (iSer && iSer.meta && iSer.meta.custom && iSer.meta.custom) {
+        const { query, data } = iSer.meta.custom;
+        if (query.queryString === 'cpu_utilization_panel') {
+          if (data) {
+            cardJSX = this.renderCPUUtilizationData(JSON.parse(data));
+          } else {
+            cardJSX = this.renderError('CPU Utilization');
+          }
+        }
+      } else {
+        cardJSX = this.renderError('');
+      }
+      retData.push(cardJSX);
+    }
+    return retData;
+  };
+
+  renderError = (cardTitle: any) => {
+    return (
+      <div className="utilization-card">
+        <div className="card-title">
+          <div className="icon">
+            <img src="" alt="" />
+          </div>
+          <span className="name">{cardTitle}</span>
+        </div>
+        <div className="utilization-details">
+          <div className="info">
+            <span className="name">There is some error.</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    const { data } = this.props;
+    if (data && data.series && data.series.length > 0) {
+      const { series } = data;
+      return <div className="appkube-utilization-panel">{this.renderFrames(series)}</div>;
+    } else {
+      return <div>No data available</div>;
+    }
   }
 }
 
